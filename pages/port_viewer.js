7da9a0ba23066887e6aa4c52383b1fc73e00f0a3
mamba72 <44649115+mamba72@users.noreply.github.com>
2021-12-09 18:24:06 -0800

    temporarily removed github repo grabber

--- pages/port_viewer.js
+++ pages/port_viewer.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-// import Link from 'next/Link'
+import Link from 'next/link'
 import { useState } from 'react'
 import { useRouter } from "next/router"; //importing for getting search queries
 
@@ -7,36 +7,37 @@ import { supabase } from '../utils/supabaseClient'
 import { Auth } from '@supabase/ui'
 
 export default function port_viewer() {
-    const { query } = useRouter(); //used to get the search query
-
+    const router = useRouter();//used to get the search query
     let GitName = "";
 
     const GetGitNameFromQuery = () =>
     {
-        GitName = query.gitName;
+        GitName = router.query.gitName;
+        console.log(GitName);
     }
 
     const GetGitHubRepos = async () => {
-        let request = require('request');
+        // let request = require('request');
     
-        let options =  {
-            url: "https://api.github.com/users/:user",
-            headers: {
-                "User-Agent": "PortGen"  // Your Github ID or application name
-            }
-        }
+        // let options =  {
+        //     url: "https://api.github.com/users/:user",
+        //     headers: {
+        //         "User-Agent": "PortGen"  // Your Github ID or application name
+        //     }
+        // }
     
-        request.get(options)
-            .on('response', function (response) {
-                console.log(response.statusCode);
-                console.log(JSON.stringify(response));
-            });v
+        // request.get(options)
+        //     .on('response', function (response) {
+        //         console.log(response.statusCode);
+        //         console.log(JSON.stringify(response));
+        //     });v
       }
 
 
     return (
         <div>
             <p>Hello, this is the portfolio viewer</p>
+            {GetGitNameFromQuery()}
         </div>
     )
 }
